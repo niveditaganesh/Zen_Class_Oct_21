7911cea5c16eaac0a32ec0ddc03950e41181b6b4
niveditaganesh <55006494+niveditaganesh@users.noreply.github.com>
2020-10-22 13:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,36 @@ for (var i = 0; i < number.length; i++) {
     }
   });
 }
+
+  document.addEventListener("keypress", function (e) {
+      if (e.keyCode === 96 || e.keyCode === 48 ) {
+          b='0'
+      }else if (e.keyCode === 97 || e.keyCode === 49 ) {
+        b='1'
+    }else if (e.keyCode === 98 || e.keyCode === 50 ) {
+      b='2'
+  }else if (e.keyCode === 99 || e.keyCode === 51 ) {
+    b='3'
+}else if (e.keyCode === 100 || e.keyCode === 52 ) {
+  b='4'
+}else if (e.keyCode === 101 || e.keyCode === 53 ) {
+  b='5'
+}else if (e.keyCode === 102 || e.keyCode === 54 ) {
+  b='6'
+}else if (e.keyCode === 103 || e.keyCode === 55 ) {
+  b='7'
+}else if (e.keyCode === 104 || e.keyCode === 56 ) {
+  b='8'
+}
+else if (e.keyCode === 105 || e.keyCode === 57 ) {
+  b='9'
+}
+else{
+  alert("invalid input")
+} var output = numberFormat(getOutput());
+if (output != NaN) {
+  output = output + b;
+  printOutput(output);
+}
+  });
+
